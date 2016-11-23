ba5327949b5a98ccbee7d7de81e3004b4c0d3df2
hasemeneh <julviar17@gmail.com>
2016-11-23 13:02:45 +0700

    first push

---
+++ application/models/CartModel.php
@@ -0,0 +1,24 @@
+<?php
+
+class TimelineModel extends CI_Model {
+	public function __construct(){
+		$this->load->database();
+	}
+	public function get_Timelines($month,$days,$year){
+		$this->db->select('nama_proker,description,badan,tanggal')->from('timeline')->where("tanggal BETWEEN '$year-$month-1' AND '$year-$month-$days'");
+		$result_data = array();
+		$result = $this->db->get()->result();
+		foreach ($result as $key => $value) {
+			$result_data[$key] = $value;
+		}
+		return $result_data;
+	} 
+	public function insertData($nama_proker,$description,$badan,$tanggal)
+	{
+		$this->db->query("INSERT INTO `timeline` (`id_proker`, `nama_proker`, `description`, `badan`, `tanggal`) VALUES (NULL, '$nama_proker', '$description', '$badan', '$tanggal');");
+
+	}
+}
+
+
+?>
---
+++ application/models/ProductModel.php
@@ -0,0 +1,25 @@
+<?php
+
+class TimelineModel extends CI_Model {
+	public function __construct(){
+		$this->load->database();
+	}
+	public function get_product($page=1){
+		$this->db->select('id,name,size_availability,price,description,material')->from('t_product')->order_by('name','asc')->(10,$page);
+		$result_data = array();
+		$result = $this->db->get()->result();
+		foreach ($result as $key => $value) {
+			$result_data[$key] = $value;
+		}
+		return $result_data;
+	} 
+	public function replace_data($name,$size_availability,$price,$description,$material,$id='NULL')
+	{
+		
+		$this->db->query("REPLACE INTO `t_product` (`id`, `name`, `size_availability`, `price`, `description`, `material`) VALUES (".$id.", '".$name."', '".$size_availability."', '"$price"', '".$description."', '".$material."');");
+		redirect("");
+	}
+}
+
+
+?>
