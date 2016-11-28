d296fd842d6f87d0a8b0363f9d02c9cea876f6e1
ahmadjihanatqia <atqiyaahmad@gmail.com>
2016-11-28 11:46:25 +0700

    CSS,JS
    edit css js

--- application/views/login.php
+++ application/views/login.php
@@ -4,9 +4,10 @@
   <title>D'Shoes</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/bootstrap.css">
-  <script src="js/jquery.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
+  
+	<script src="<?php echo base_url();?>/assets/js/jquery.min.js"></script>
+	<script src="<?php echo base_url();?>/assets/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/assets/css/bootstrap.css">
 </head>
 <body>
 
