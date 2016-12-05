e118acc0de31a9e04af39798d867f4b40532da95
ahmadjihanatqia <atqiyaahmad@gmail.com>
2016-12-05 09:48:56 +0700

    Tampilan

--- application/views/cart.php
+++ application/views/cart.php
@@ -64,9 +64,9 @@
 						<div class="panel-body ">Are you sure want to delete the data?
 							<form action="<?php echo base_url().'index.php/Product/deletefromcart';?>"  method="POST">
 								<input type="hidden" name="id" value="<?php echo $key->id;?>">
-								<button type="submit" class='btn btn-danger'>Yes</button>
+								<button style="float:left;margin-right:5px" type="submit" class='btn btn-danger'>Yes</button>
 							</form>							
-							<button type=button class='btn btn-default' data-toggle='collapse' data-target='#collapse<?php echo $no++;?>'>No</button>
+							<button style="float:left" type=button class='btn btn-default' data-toggle='collapse' data-target='#collapse<?php echo $no++;?>'>No</button>
 						</div>
 					</div>
 					</td>
@@ -81,22 +81,28 @@
 						<td>Rp12345678</td>
 						<td>
 						<button type=button class='col-sm-12 btn btn-sm btn-success' data-toggle='collapse' data-target='#collapse999'>Buy</button>
-						<div id="collapse999" style="background-color:rgb(183, 239, 179);" class="panel-collapse collapse">
-							<div class="panel-body ">Are you sure want to check out the cart?
-								<form action="<?php echo base_url().'index.php/Product/verifytransaction';?>"  method="POST">
-									<input type="hidden" name="id" value="<?php echo $transaction[0]->id;?>">
-									<input type="text" name="alamat" placeholder="Masukan Alamat Anda" required>
-									<button type="submit" class='btn btn-danger'>Yes</button>
-								</form>							
-								<button type=button class='btn btn-default' data-toggle='collapse' data-target='#collapse999'>No</button>
-							</div>
-						</div>
+						
 						</td>
+						</tr>
+						<tr>
+							<td colspan=5 style="padding:0px">
+								<div id="collapse999" style="background-color:rgb(183, 239, 179);" class="panel-collapse collapse">
+								<div class="panel-body ">Are you sure want to check out the cart?
+									<form action="<?php echo base_url().'index.php/Product/verifytransaction';?>"  method="POST">
+										<input type="hidden" name="id" value="<?php echo $transaction[0]->id;?>">
+											<div class="form-group">
+												<textarea class="form-control"  rows="5" cols="12" type="text" name="alamat" placeholder="Masukan Alamat Anda" required></textarea>
+											</div>
+										<button style="float:left;margin-right:5px" type="submit" class='btn btn-danger'>Yes</button>
+									</form>							
+									<button style="float:left;" type=button class='btn btn-default' data-toggle='collapse' data-target='#collapse999'>No</button>
+								</div>
+								</div>
+							</td>		
+						</tr>
 						<?php
 					}
 				?>
-				</tr>
-
 			</table>
 		</div> 
 	</div>
